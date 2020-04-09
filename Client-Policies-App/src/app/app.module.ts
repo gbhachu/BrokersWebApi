ab8fe550fe5e40af416f6a60488bfc572d24de19
gbhachu <gurminderbhachu@hotmail.com>
2020-04-09 22:47:38 +0100

    client-detail component forms set up for client and policy

--- Client-Policies-App/src/app/app.module.ts
+++ Client-Policies-App/src/app/app.module.ts
@@ -2,9 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { ClientDetailsComponent } from './client-details/client-details.component';
 import { ClientDetailComponent } from './client-details/client-detail/client-detail.component';
 import { ClientDetailPolicyListComponent } from './client-details/client-detail-policy-list/client-detail-policy-list.component';
+import { ClientDetailService } from './shared/client.service';
+import { PolicyService } from './shared/policy.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,11 @@ import { ClientDetailPolicyListComponent } from './client-details/client-detail-
     ClientDetailPolicyListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ClientDetailService, PolicyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
